747c5529bb8b10f3510218241ee3b962131f2a04
tino415 <cernakmartin3@gmail.com>
2025-07-06 22:43:41 +0200

    Initial commit

---
+++ semaphoreci-debug.el
@@ -0,0 +1,22 @@
+(require 'tramp)
+
+(defun semaphoreci-exec (job)
+  (interactive "sJob: \n")
+  (thread-last
+    (string-replace "sem debug job " "" job)
+    (format "/sem:%s:~")
+    (call-interactively #'execute-extended-command)))
+
+(add-to-list 'tramp-methods
+             '("sem"
+               (tramp-login-program "sh")
+               (tramp-login-args
+                (("-c")
+                 ("'sem") ("attach") ("%h")
+                 ("||")
+                 ("sem") ("debug") ("job") ("%h")
+                 ("'")))
+               (tramp-remote-shell "/bin/sh")
+               (tramp-remote-shell-args ("-c"))))
+
+(provide 'semaphoreci-debug)
