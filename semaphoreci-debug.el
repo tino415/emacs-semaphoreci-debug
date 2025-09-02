6a8926a6c4043271a5f1425bb4be02aa32a97bcf
tino415 <cernakmartin3@gmail.com>
2025-09-02 18:00:49 +0200

    Refactor start command and automatic dired

--- semaphoreci-debug.el
+++ semaphoreci-debug.el
@@ -1,22 +1,29 @@
 ;;; semaphoreci-debug.el --- Debug SemaphoreCI via TRAMP  -*- lexical-binding: t; -*-
 (require 'tramp)
 
-(defun semaphoreci-exec (job)
+(defun semaphoreci-start (job)
   (interactive "sJob: \n")
-  (thread-last
-    (string-replace "sem debug job " "" job)
-    (format "/sem:%s:~")
-    (call-interactively #'execute-extended-command)))
+  (let* ((job (string-replace "sem debug job " "" job))
+         (process-name (format "sem-debug-job-%s" job))
+         (buffer-name (format "*%s*" process-name))
+         (buffer (get-buffer-create buffer-name))
+         (folder (format "/sem:%s:~" semaphoreci-job)))
+    (make-process
+     :name process-name
+     :buffer buffer
+     :command (list "sem" "debug" "job" job-id)
+     :filter (lambda (proc output)
+               (with-current-buffer (process-buffer proc)
+                 (insert output)
+                 (when (string-match "Semaphore CI Debug Session\\." output) (dired folder))))
+     :sentinel (lambda (proc event)
+                 (when (not (process-live-p proc))
+                   (message "sem debug job %s exited: %s" job event))))))
 
 (add-to-list 'tramp-methods
              '("sem"
-               (tramp-login-program "sh")
-               (tramp-login-args
-                (("-c")
-                 ("'sem") ("attach") ("%h")
-                 ("||")
-                 ("sem") ("debug") ("job") ("%h")
-                 ("'")))
+               (tramp-login-program "sem")
+               (tramp-login-args (("attach") ("%h")))
                (tramp-remote-shell "/bin/sh")
                (tramp-remote-shell-args ("-c"))))
 
