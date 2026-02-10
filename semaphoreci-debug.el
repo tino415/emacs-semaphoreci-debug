040e6be8650a358dca1970bd5b0ea23ce623dc45
tino415 <cernakmartin3@gmail.com>
2026-02-10 09:39:23 +0100

    Fix job id changed on attach

--- semaphoreci-debug.el
+++ semaphoreci-debug.el
@@ -1,24 +1,46 @@
-;;; semaphoreci-debug.el --- Debug SemaphoreCI via TRAMP  -*- lexical-binding: t; -*-
+;;; semaphoreci-debug.el --- Debug SemaphoreCI jobs  -*- lexical-binding: t; -*-
 (require 'tramp)
+(require 'transient)
+
+(defvar-local semaphoreci-job-id nil)
+(defvar-local semaphoreci-job-id-requested nil)
+
+(defconst semaphoreci--job-id-regexp
+  "\\([0-9a-fA-F]\\{8\\}-[0-9a-fA-F]\\{4\\}-[0-9a-fA-F]\\{4\\}-[0-9a-fA-F]\\{4\\}-[0-9a-fA-F]\\{12\\}\\)")
+
+(defun semaphoreci--ask-job-id (output)
+  (when (and (not semaphoreci-job-id-requested)
+             (string-match-p comint-prompt-regexp output))
+    (setq semaphoreci-job-id-requested t)
+    (comint-send-string
+     (get-buffer-process (current-buffer))
+     "echo $SEMAPHORE_JOB_ID\n"))
+  output)
+
+(defun semaphoreci--extract-job-id (output)
+  (when (and semaphoreci-job-id-requested
+             (not semaphoreci-job-id)
+             (string-match semaphoreci--job-id-regexp output))
+    (setq semaphoreci-job-id (match-string 1 output))
+    (setq default-directory
+          (format "/sem:%s:~/" semaphoreci-job-id)))
+  output)
+
 
 (defun semaphoreci-start (job)
-  (interactive "sJob: \n")
+  (interactive "sJob: ")
   (let* ((job (string-replace "sem debug job " "" job))
-         (process-name (format "sem-debug-job-%s" job))
-         (buffer-name (format "*%s*" process-name))
-         (buffer (get-buffer-create buffer-name))
-         (folder (format "/sem:%s:~" job)))
-    (make-process
-     :name process-name
-     :buffer buffer
-     :command (list "sem" "debug" "job" job)
-     :filter (lambda (proc output)
-               (with-current-buffer (process-buffer proc)
-                 (insert output)
-                 (when (string-match "Semaphore CI Debug Session\\." output) (dired folder))))
-     :sentinel (lambda (proc event)
-                 (when (not (process-live-p proc))
-                   (message "sem debug job %s exited: %s" job event))))))
+         (buffer-name (format "*sem-debug %s*" job))
+         (buffer (get-buffer-create buffer-name)))
+    (with-current-buffer buffer
+      (comint-mode)
+      (add-hook 'comint-preoutput-filter-functions
+                #'semaphoreci--ask-job-id nil t)
+      (add-hook 'comint-preoutput-filter-functions
+                #'semaphoreci--extract-job-id nil t)
+      (make-comint-in-buffer buffer-name buffer
+                              "sem" nil "debug" "job" job))
+    (switch-to-buffer buffer)))
 
 (add-to-list 'tramp-methods
              '("sem"
